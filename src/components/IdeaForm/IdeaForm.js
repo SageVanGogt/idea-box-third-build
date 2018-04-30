f6856d2ef5f9b600f72d543dfe6c69455a7faffe
Sage Vogt <thurmanvogt@gmail.com>
2018-04-30 11:08:00 -0600

    Add ideaForm component with updating state

---
+++ src/components/IdeaForm/IdeaForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class IdeaForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: '',
+      quality: 'swill'
+    }
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    return(
+      <form>
+        <input 
+          type="text"
+          placeholder="title"
+          onChange={this.handleInputChange}
+          value={this.state.title}
+          name='title'
+        />
+        <input 
+          type="text"
+          placeholder="body"
+          onChange={this.handleInputChange}
+          value={this.state.body}
+          name='body'
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default IdeaForm;
